c6a31a7e2a6e05f2d744757b83ee9f80a4320e59
Pedro Fonseca Nunes <pedrofonunes@gmail.com>
2024-11-18 13:30:41 -0300

    feat: ajustando arquitetura para que cade classes, pacote e sufixo estejam em sua devida classe.

---
+++ src/test/kotlin/com/pefonseca/hexagonal/architecture/NameConventionTest.kt
@@ -0,0 +1,33 @@
+package com.pefonseca.hexagonal.architecture
+
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+
+@AnalyzeClasses(packages = ["com.pefonseca.hexagonal"])
+class NameConventionTest {
+
+    @ArchTest
+    val `consumer reside only consumer package` = classes()
+        .that()
+        .haveNameMatching(".*Consumer")
+        .should()
+        .resideInAPackage("..adapters.in.consumer")
+        .`as`("Consumer classes should reside inside consumer package in adapters.in package")
+
+    @ArchTest
+    val `resonse reside only response package` = classes()
+        .that()
+        .haveNameMatching(".*Response")
+        .should()
+        .resideInAnyPackage("..adapters.in.controller.response", "..adapters.out.client.response")
+        .`as`("Response classes should reside inside response package in controller or client packages")
+
+    @ArchTest
+    val `should be suffixed consumer` = classes()
+        .that()
+        .resideInAPackage("..consumer")
+        .should()
+        .haveSimpleNameEndingWith("Consumer")
+
+}
